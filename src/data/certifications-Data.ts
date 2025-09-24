55a36d289bb2f2a492c92cbdd6261ec92f492c5f
Mario <mariocode616@gmail.com>
2025-09-24 14:35:47 -0500

    Describe brevemente lo que cambiaste

--- src/data/certifications-Data.ts
+++ src/data/certifications-Data.ts
@@ -4,7 +4,7 @@ export const certs = [
     name: "React: De cero a experto - Edición 2025",
     org: "Udemy",
     year: "2025",
-    image: "./public/certificaciones/certificado-react.jpg",
+    image: "/certificado-react.jpg",
     pdf: "./certificado-react.pdf",
     link: "https://www.udemy.com/certificate/UC-46a20146-c284-410e-9e47-c71932209d34"
   }
