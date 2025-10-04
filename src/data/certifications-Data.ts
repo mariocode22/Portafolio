a86566e070eb1f0c68967fe4607f748920f639e7
Mario <mariocode616@gmail.com>
2025-10-04 15:18:11 -0500

    nuevo certificado

--- src/data/certifications-Data.ts
+++ src/data/certifications-Data.ts
@@ -7,5 +7,13 @@ export const certs = [
     image: "/certificado-react.jpg",
     pdf: "./certificado-react.pdf",
     link: "https://www.udemy.com/certificate/UC-46a20146-c284-410e-9e47-c71932209d34"
+  },
+   {
+    name: "Curso de Iniciacion al Desarrollo con IA",
+    org: "BIG School",
+    year: "2025",
+    image: "/certificado-ia.png",
+    pdf: "",
+    link: ""
   }
 ];
